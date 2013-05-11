9d8034d9074984789cc9101f8fd5576c1ac0c4e9
Zbigniew Siciarz <antyqjon@gmail.com>
2013-05-11 19:41:59 +0200

    Deserialize project from file in the project_widget example.

--- Examples/project_widget/ProjectWidgetDemoWindow.cpp
+++ Examples/project_widget/ProjectWidgetDemoWindow.cpp
@@ -1,5 +1,8 @@
 #include "ProjectWidgetDemoWindow.h"
+#include "Required/Project/ProjectSerializer.h"
+#include "Required/Project/ProjectException.h"
 #include "Required/Project/FileCategory.h"
+#include <QFile>
 #include <QRegExp>
 
 ProjectWidgetDemoWindow::ProjectWidgetDemoWindow(QWidget *parent) :
@@ -8,8 +11,29 @@ ProjectWidgetDemoWindow::ProjectWidgetDemoWindow(QWidget *parent) :
     Required::FileCategory::registerCategory("txt", "Text files", QRegExp("*.txt", Qt::CaseSensitive, QRegExp::Wildcard));
     Required::FileCategory::registerCategory("json", "JSON data", QRegExp("*.json", Qt::CaseSensitive, QRegExp::Wildcard));
 
-    m_project = new Required::Project();
-    m_project->setName("My Awesome Project");
+    // load project data from file
+    QFile file("project.xml");
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        Required::ProjectSerializer serializer(&file);
+        try
+        {
+            m_project = serializer.deserialize();
+        }
+        catch (Required::ProjectException& e)
+        {
+            m_project = new Required::Project(this);
+            m_project->setName("My Awesome Project");
+        }
+        file.close();
+    }
+    else
+    {
+        m_project = new Required::Project(this);
+        m_project->setName("My Awesome Project");
+    }
+
+    m_project->setParent(this);
     m_projectWidget = new Required::ProjectWidget(this);
     m_projectWidget->setProject(m_project);
     setCentralWidget(m_projectWidget);
